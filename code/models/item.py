1559ce06adb082b39f51bd8c284c758bc40bb4d1
Yuriy_Kazan <kazan.yura@gmail.com>
2021-06-30 19:12:45 +0300

    Add project files

---
+++ code/models/item.py
@@ -0,0 +1,35 @@
+from db import db
+
+
+class ItemModel(db.Model):
+    __tablename__ = 'items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    price = db.Column(db.Float(precision=2))
+
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
+    store = db.relationship('StoreModel')
+
+    quantity = db.Column(db.Integer, default=0)
+
+    def __init__(self, name, price, store_id, quantity=0):
+        self.name = name
+        self.price = price
+        self.store_id = store_id
+        self.quantity = quantity
+
+    def json(self):
+        return {'name': self.name, 'price': self.price, 'quantity': self.quantity}
+
+    @classmethod
+    def find_by_name(cls, name):
+        return ItemModel.query.filter_by(name=name).first()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
---
+++ code/resources/item.py
@@ -0,0 +1,69 @@
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+from models.item import ItemModel
+
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+                        type=float,
+                        required=True,
+                        help='The field can\'t be empty')
+    parser.add_argument('store_id',
+                        type=int,
+                        required=True,
+                        help='Each item has to have store id')
+    parser.add_argument('quantity',
+                        type=int,
+                        required=False,
+                        help='Each item has to have store id')
+
+    @jwt_required()
+    def get(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            return item.json()
+        return {'message': 'Item not found'}, 404
+
+    def post(self, name):
+        if ItemModel.find_by_name(name):
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400
+
+        data = Item.parser.parse_args()
+        item = ItemModel(name, **data)
+
+        try:
+            item.save_to_db()
+        except ValueError():
+            return {'message': 'An error occurred inserting item'}, 500
+
+        return item.json(), 201
+
+    def delete(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
+        else:
+            return {'message': 'The item was not find'}
+        return {'message': 'The item deleted'}, 202
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = ItemModel.find_by_name(name)
+
+        if item is None:
+            item = ItemModel(name, **data)
+        else:
+            item.price = data['price']
+            item.store_id = data['store_id']
+            if data['quantity']:
+                item.quantity = data['quantity']
+            else:
+                item.quantity = 0
+        item.save_to_db()
+        return item.json()
+
+
+class ItemList(Resource):
+    def get(self):
+        return {"items": list(map(lambda x: x.json(), ItemModel.query.all()))}
