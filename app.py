b483392eea8a9179c8767220fdd1e943ada056c8
Yuriy_Kazan <kazan.yura@gmail.com>
2021-07-01 01:15:30 +0300

    Move db import higher

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from flask import Flask
 from flask_jwt import JWT
 from flask_restful import Api
 
+from db import db
 from resources.item import Item, ItemList
 from resources.user import UserRegister
 from resources.store import Store, StoreList
 from security import authenticate, identity
-from db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
